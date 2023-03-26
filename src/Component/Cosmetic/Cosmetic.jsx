3489b48641cf8f38079764ceaee76ec56ff70950
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-26 11:35:32 -0700

    even handler add

--- src/Component/Cosmetic/Cosmetic.jsx
+++ src/Component/Cosmetic/Cosmetic.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
-    const {name,price,picture,_id} =props.cosmetic
+    const {name,price,picture,_id} =props.cosmetic;
+    const addToCart=(_id)=>{
+       console.log('item added',_id)
+    }
     return (
-        <div>
+        <div className='cosmetic'>
             <h3>Name: {name}</h3>
             <img src={picture} alt="" />
             <p>it has id:{_id}</p>
             <p>Price :{price}</p>
+            <button onClick={()=>{addToCart(_id)}}>Add to Cart</button>
+            
         </div>
     );
 };
