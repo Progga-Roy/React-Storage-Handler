033406e518e346cbd8f727478226953a7f057b4f
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-26 11:01:00 -0700

    create fake API

--- src/Component/Cosmetic/Cosmetic.jsx
+++ src/Component/Cosmetic/Cosmetic.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Cosmetic = (props) => {
-    const {name,price} =props.cosmetic
+    const {name,price,picture,_id} =props.cosmetic
     return (
         <div>
             <h3>Name: {name}</h3>
+            <img src={picture} alt="" />
+            <p>it has id:{_id}</p>
             <p>Price :{price}</p>
         </div>
     );
--- src/Component/Cosmetics/Cosmetics.jsx
+++ src/Component/Cosmetics/Cosmetics.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 // import { add } from '../utilities/Calculation';
 // import add from '../utilities/Calculation';
 
 const Cosmetics = () => {
-    const cosmetics =[
-        {id:1, name:'Alta', price: 100},
-        {id:2, name:'Nail-Palish', price: 200},
-        {id:3, name:'Lipstick', price: 300},
-        {id:4, name:'Kajol', price: 400},
-        {id:5, name:'Facepowder', price: 500},
+   const [cosmetics,setCosmetics] =useState([])
+   useEffect(()=>{
+    fetch('data.json')
+    .then(res=>res.json())
+    .then(data=>setCosmetics(data))
 
-    ]
+   },[])
     // const first = 55;
     // const second = 30;
     // const total = add(first,second)
@@ -20,7 +19,7 @@ const Cosmetics = () => {
             {/* <h2>My Cosmetics</h2>
             <p>Total : {total}</p> */}
          {
-            cosmetics.map(cosmetic=><Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>)
+            cosmetics.map(cosmetic=><Cosmetic key={cosmetic._id} cosmetic={cosmetic}></Cosmetic>)
          }
 
         </div>
